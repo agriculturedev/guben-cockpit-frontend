5930e06f77bc01eeb516fd173758589bfa6e5e6b
Jonas Faber <jonas.h.faber@gmail.com>
2024-05-31 14:36:43 +0200

    add basic styling to GenericButton

--- src/components/DynamicRenderer.vue
+++ src/components/DynamicRenderer.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <ul>
-      <component
-        :is="getComponentByType(component.__component)"
-        v-for="component in components"
-        :key="component.id"
-        :values="component"
-      />
-    </ul>
+  <div class="dynamic-renderer">
+    <component
+      :is="getComponentByType(component.__component)"
+      v-for="component in components"
+      :key="component.id"
+      :values="component"
+    />
   </div>
 </template>
 
@@ -32,4 +30,10 @@ export default defineComponent({
 
 <style scoped>
 /* Add any styles specific to the error component here */
+.dynamic-renderer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+}
 </style>
--- src/components/Generic/Button.vue
+++ src/components/Generic/Button.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="button">
-    <a :href="values.URL">{{ values.Text }}</a>
+    <a :href="values.URL" :target="values.newTab ? '_blank' : ''">{{
+      values.Text
+    }}</a>
   </div>
 </template>
 
@@ -21,4 +23,25 @@ export default defineComponent({
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.button {
+  a {
+    transition: background-color 0.3s, color 0.3s;
+    background-color: white;
+    border: 1px solid #89a5d3;
+    color: #89a5d3;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 10px;
+    box-shadow: 0 5px 12px -4px #999;
+
+    &:hover {
+      background-color: #1f4b84;
+      color: white;
+    }
+  }
+}
+</style>
