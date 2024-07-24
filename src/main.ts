13a7e2f4e64ee7fe403cd41edee834d4d9a3196e
LiamD-Flop <40887493+LiamD-Flop@users.noreply.github.com>
2024-07-24 19:46:08 +0200

    add ClickOutsideElement event

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import clickOutsideDirective from "./helpers/clickOutsideDirective";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .directive("click-outside", clickOutsideDirective)
+  .mount("#app");
