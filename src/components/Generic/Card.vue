155debaedb69af81739699f8a24d8825cbeab4d0
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-08-15 18:17:51 +0200

    make card simpler, helps with correct styling of content

--- src/components/Generic/Card.vue
+++ src/components/Generic/Card.vue
@@ -6,12 +6,7 @@
 
 <style lang="scss" scoped>
 .card {
-  padding: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+  width: fit-content;
   border-radius: 5px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 }
