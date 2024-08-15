89bc97e3450ca2c83a3b3021d3cfa9ea3cf65a92
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-08-15 18:16:28 +0200

    update homepage to use strapi data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="home-container max-width">
-    <h2 class="title">{{ title }}</h2>
-    <VueMarkdown
-      :source="description"
-      :options="markdownOptions"
-      class="description"
-    />
-    <div class="tab-panel">
-      <TabPanel />
+    <div class="">
+      <ErrorComponent :loading="loading" :error="error">
+        <template #content>
+          <div v-if="homePage">
+            <h2 class="title">{{ homePage.attributes.title }}</h2>
+
+            <VueMarkdown
+              :source="homePage.attributes.description"
+              :options="markdownOptions"
+              class="description"
+            />
+            <TabPanel class="tab-panel" :tabs="homePage.attributes.tabs.data" />
+          </div>
+          <div v-else>No home page data available.</div>
+        </template>
+        <template #error>
+          <div>There was an error loading the home page data.</div>
+        </template>
+      </ErrorComponent>
     </div>
   </div>
 </template>
@@ -39,21 +50,37 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import VueMarkdown from "vue-markdown-render";
+import { HomeView } from "@/types/HomeView";
+import ErrorComponent from "@/components/ErrorComponent.vue";
+import { fetchHomePage } from "@/api/ApiService";
 import TabPanel from "@/components/Home/TabPanelComponent.vue";
 
 export default defineComponent({
   name: "HomeView",
-  components: { TabPanel, VueMarkdown },
+  components: {
+    TabPanel,
+    ErrorComponent,
+    VueMarkdown,
+  },
   data() {
     return {
-      title: "Wilkommen auf der Übersichtsseite",
-      description:
-        "Herzlich willkommen auf dem Dashboard des Guben Cockpit! Um barrierefreie und grenzüberschreitende Zusammenarbeit zu vereinfachen, digitalisiert Guben städtische Aktivitäten und Informationen. Seien Sie eingeladen die städtische Daten frei nach Ihren Interessen  zu erkunden und für Ihre Ideen einzusetzen. Das Ziel: Guben gemeinsam mit Ihnen zu entwickeln. Lassen Sie uns Guben gemeinsam gestalten unter dem Motto: Eine smarte Europastadt.",
+      homePage: null as HomeView | null,
+      loading: true,
+      error: null,
       markdownOptions: {
         underline: true,
         italic: true,
       },
     };
+  },
+  async mounted() {
+    try {
+      this.homePage = (await fetchHomePage()) as HomeView;
+    } catch (error) {
+      this.error = error as never;
+    } finally {
+      this.loading = false;
+    }
   },
 });
 </script>
